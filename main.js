8b4b6cf34de95e4c3306863953fdef4718d8716a
Lossttt <lfatmi.ayoub@gmail.com>
2024-05-28 23:41:49 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import './style.css'
-import { fetchDataOrder, fetchDataOmzet, countOrdersPerYear, calculateRevenuePerYear } from './data.js';
+import './style.css';
+import 'chartjs-adapter-date-fns';
+import { fetchDataOrder, fetchDataOmzet, countOrdersPerYear, calculateRevenuePerYear, countTransactions } from './data.js';
 
 //Standaard main js bestand waar alles samen komt
 
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", async function() {
 
   countOrdersPerYear(orderData, 'year');
   calculateRevenuePerYear(omzetData, 'year');
+  countTransactions(omzetData, 'year');
 });
 
 document.getElementById('viewSelect').addEventListener('change', async function() {
@@ -22,4 +24,9 @@ document.getElementById('viewSelect').addEventListener('change', async function(
 document.getElementById('viewSelectOmzet').addEventListener('change', async function() {
   const yearSelect = document.getElementById('viewSelectOmzet').value;
   calculateRevenuePerYear(omzetData, yearSelect);
+});
+
+document.getElementById('viewSelectTransactions').addEventListener('change', async function() {
+  const yearSelect = document.getElementById('viewSelectTransactions').value;
+  countTransactions(omzetData, yearSelect);
 });
